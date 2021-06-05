053249a94f810caffc96643ba69ddd547047c8b7
fraserjohnstone <fraserjohnstone12345@hotmail.com>
2021-06-05 09:50:38 +0100

    improve the producer by accepting an event type

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -30,31 +30,4 @@ class GraphqlController < ApplicationController
       }
     }.to_json
   end
-
-  def available_session_response
-    {
-      "data": {
-        "getPanelInvitation": {
-          "email": "participant@usertesting.dev",
-          "currentLocale": "en-US",
-          "displayName": "Person Name",
-          "dashboardUrl": "http://localhost:3000/my_dashboard",
-          "unsubscribeUrl": "http://localhost:3000/unsubscribe?id=etc",
-          "availableSession": {
-            "panelTitle": "Dynademo Panel",
-            "compensationAmount": 10,
-            "otherRequirement": nil,
-            "nextScreenerQuestion": nil,
-            "panel": {
-              "title": "Our Panel",
-              "type": "my_panel",
-              "brandingAsset": {
-                "presignedUrl": "https://a_long_url.com/image.png"
-              }
-            }
-          }
-        }
-      }
-    }.to_json
-  end
 end
--- app/producers/mock_producer.rb
+++ app/producers/mock_producer.rb
@@ -3,14 +3,23 @@ class MockProducer
   KAFKA_BROKERS = ["kafka:9092"].freeze
   KAFKA_CLIENT_ID = "mock-orders".freeze
 
-  def self.produce!
-    kafka = Kafka.new(KAFKA_BROKERS, client_id: KAFKA_CLIENT_ID)
-    producer = kafka.producer
-    producer.produce(message_body.to_json, topic: KAFKA_TOPIC)
+  def self.produce!(event_type)
+    kafka_client = Kafka.new(KAFKA_BROKERS, client_id: KAFKA_CLIENT_ID)
+    producer = kafka_client.producer
+    producer.produce(message_body(event_type), topic: KAFKA_TOPIC)
     producer.deliver_messages
   end
 
-  def self.message_body
+  def self.message_body(event_type)
+    case event_type
+    when :panel_invitation_requested
+      panel_invitation_requested_body
+    else
+      {}
+    end
+  end
+
+  def self.panel_invitation_requested_body
     {
       "type" => "PANEL_INVITATION_REQUESTED",
       "data" => {
@@ -20,6 +29,6 @@ class MockProducer
           "timestamp"=>"2021-06-03T04:19:50.131127-07:00"
         }
       }
-    }
+    }.to_json
   end
 end
